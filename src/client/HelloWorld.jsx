b045d3c3541f9c7211cb6e768d106d310b5bd210
Thatsaphon Losuwannarak <157302375+thatsaphon2745@users.noreply.github.com>
2024-09-26 10:43:30 +0000

    FirstCommit

---
+++ src/client/HelloWorld.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect, useRef } from "react";
+
+export default function HelloWorld(){
+    return (
+        <>
+            <h1>Hello World Client</h1>
+        </>
+    );
+}
